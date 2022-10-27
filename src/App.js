f496d78440c31e4cbb6219854db492eb193aeb26
hasan <mdhasanhk.98@gmail.com>
2022-10-27 13:47:56 +0600

    register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -11,6 +11,10 @@ function App() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('')
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState('')
+
   const handleNameBlur = (e) => {
     setName(e.target.value)
   };
@@ -21,9 +25,32 @@ function App() {
     setPassword(e.target.value)
   };
 
+  const handleRegisteredChange =(e) => {
+    setRegistered(e.target.checked)
+  }
   const handleFormSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
+    setValidated(true);
+
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
+    if(registered) {
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user= result.user
+        console.log(user)
+        setError('')
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
         setUserName();
@@ -31,7 +58,9 @@ function App() {
       })
       .catch(error => {
         console.error(error)
-      })
+        setError(error.message)
+      });
+    }
   };
 
   const setUserName = () => {
@@ -47,30 +76,40 @@ function App() {
   };
   return (
     <div className='w-50 mx-auto'>
-      <h3 className='text-primary'>Please register</h3>
-      <Form onSubmit={handleFormSubmit}>
+      <h3 className='text-primary'>Please {!registered ? 'Register' : 'Login'}</h3>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Your name</Form.Label>
-          <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter your name" />
+          <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter your name" required />
+          <Form.Control.Feedback type="invalid">
+            Please choose a valid name.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
+        <p className='text-danger'>{error}</p>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered?" />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          {!registered ? 'Register' : 'Login'}
         </Button>
       </Form>
     </div>
